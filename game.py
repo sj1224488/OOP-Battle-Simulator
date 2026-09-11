692bab5ce32b12d776aba4625c556693fbe91dfd
sj1224488 <shubha.jindal@ascte.org>
2026-09-11 12:50:34 -0500

    Add New Goblin
    Named Tibble

--- game.py
+++ game.py
@@ -13,6 +13,11 @@ def main():
     goblin = Goblin("Gribble")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+
+    goblinTwo = Goblin("Tibble")
+
+    print(f"{goblinTwo.name} enters the arena with {goblin.health} health.")
+
     print("But no hero has answered the call... yet.")
 
 
