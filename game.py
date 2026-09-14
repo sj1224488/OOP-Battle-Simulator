f5173239f039a9b5b01fa9e77e8f00aa76beb26d
sj1224488 <shubha.jindal@ascte.org>
2026-09-14 10:36:51 -0500

    Build Hero Class

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "The Iron Rectangle"
 
@@ -19,6 +19,17 @@ def main():
     print(f"{goblinTwo.name} enters the arena with {goblin.health} health.")
 
     print("But no hero has answered the call... yet.")
+
+    hero = Hero("Nya","Ninja")
+
+    print(f"{hero.name} the {hero.hero_class} enter the arena with {hero.health} health.")
+
+    heroAttack = hero.attack()
+    goblin.take_damage(heroAttack)
+
+    if goblin.is_alive():
+        goblinAttack = goblin.attack()
+        hero.take_damage(goblinAttack)
 
 
 if __name__ == "__main__":
--- hero.py
+++ hero.py
@@ -1,4 +1,25 @@
+import random
+
 class Hero:
     """The hero blueprint will be implemented later in the project."""
 
-    pass
+    def __init__(self, name,hero_class):
+        self.name = name
+        self.health = 130
+        self.attack_power = 10
+        self.hero_class = hero_class
+
+    def attack(self):
+        return random.randint(1, self.attack_power)
+
+    def take_damage(self, damage):
+        """Reduce health without allowing it to fall below zero."""
+        self.health = max(0, self.health - damage)
+        print(f"{self.name} takes {damage} damage. Health: {self.health}")
+        
+
+    def is_alive(self):
+        """Return True while the goblin has health remaining."""
+        return self.health > 0
+
+
