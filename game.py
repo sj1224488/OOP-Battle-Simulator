65976b3619f1b38897696cf9761e60c0e9d3c033
sj1224488 <shubha.jindal@ascte.org>
2026-09-15 09:48:54 -0500

    Created A Battle Loop
    Deleted The Singular Attack From Each Character and instead made if a fight to the death!!!

--- game.py
+++ game.py
@@ -1,8 +1,19 @@
 from goblin import Goblin
 from hero import Hero
 
-ARENA_NAME = "The Iron Rectangle"
+ARENA_NAME = "The Iron Chain"
 
+def battle(warrior: Hero,enemy: Goblin):
+    while warrior.is_alive() and enemy.is_alive():
+        warrior_damage = warrior.attack()
+        enemy.take_damage(warrior_damage)
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            warrior.take_damage(enemy_damage) 
+    if warrior.is_alive():
+        print(f"{warrior.name} wins!")
+    else:
+        print(f"{enemy.name} wins!")
 
 def main():
     """Open the arena and introduce its first opponent."""
@@ -11,25 +22,16 @@ def main():
     print("The gates are opening...")
 
     goblin = Goblin("Gribble")
-
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
-
     goblinTwo = Goblin("Tibble")
-
     print(f"{goblinTwo.name} enters the arena with {goblin.health} health.")
 
+
     print("But no hero has answered the call... yet.")
-
     hero = Hero("Nya","Ninja")
-
     print(f"{hero.name} the {hero.hero_class} enter the arena with {hero.health} health.")
 
-    heroAttack = hero.attack()
-    goblin.take_damage(heroAttack)
-
-    if goblin.is_alive():
-        goblinAttack = goblin.attack()
-        hero.take_damage(goblinAttack)
+    battle(hero,goblin)
 
 
 if __name__ == "__main__":
